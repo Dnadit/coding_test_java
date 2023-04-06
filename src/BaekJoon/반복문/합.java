169f4fdfeb140f12c2e7ea86f03ae7c4cccad5c3
Dnadit <yuhyun.kang07@gmail.com>
2023-04-06 23:34:41 +0900

    반복문 4/12 완료

--- src/BaekJoon/반복문/합.java
+++ src/BaekJoon/반복문/합.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 구구단 {
+public class 합 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        for (int i = 1; i < 10; i++)
-            System.out.println(n + " * " + i + " = " + n*i);
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        System.out.println(result);
     }
 }
