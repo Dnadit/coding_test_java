1e87d74796e20357e2e3288f597c65a1267e350f
Dnadit <yuhyun.kang07@gmail.com>
2023-04-12 13:43:12 +0900

    반복문 8문제 완료
    - 백준알고리즘 반복문 8문제 완료

---
+++ src/BaekJoon/반복문/AplusBminus7.java
@@ -0,0 +1,21 @@
+package BaekJoon.반복문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AplusBminus7 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int result = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            System.out.println("Case #" + i + ": " + result);
+        }
+    }
+}
